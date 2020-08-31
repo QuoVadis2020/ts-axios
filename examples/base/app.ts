4dcba62462445ada87c68de03804dc3269b20b25
QuoVadis2020 <im.mirror@live.cn>
2020-09-01 00:11:40 +0800

    feat(header): set header

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -70,26 +70,25 @@ axios({
   }
 })
 
-// axios({
-//   method: 'post',
-//   url: '/base/post',
-//   headers: {
-//     'content-type': 'application/json;charset=utf-8'
-//   },
-//   data: {
-//     a: 1,
-//     b: 2
-//   }
-// })
-//
-// const arr = new Int32Array([21, 31])
-//
-// axios({
-//   method: 'post',
-//   url: '/base/buffer',
-//   data: arr
-// })
-//
+axios({
+  method: 'post',
+  url: '/base/post',
+  headers: {
+    'content-type': 'application/json;charset=utf-8'
+  },
+  data: {
+    a: 1,
+    b: 2
+  }
+})
+
+const arr = new Int32Array([21, 31])
+
+axios({
+  method: 'post',
+  url: '/base/buffer',
+  data: arr
+})
 //
 // const paramsString = 'q=URLUtils.searchParams&topic=api'
 // const searchParams = new URLSearchParams(paramsString)
